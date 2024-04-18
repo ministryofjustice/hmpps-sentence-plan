8f179736a0ac6b13f9a18edbaf67665548725145
Pankaj <92032149+pk2453@users.noreply.github.com>
2024-04-18 09:16:27 +0100

    SP2-267: create initial database (#4)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
   id("uk.gov.justice.hmpps.gradle-spring-boot") version "5.15.3"
-  kotlin("plugin.spring") version "1.9.22"
+  kotlin("plugin.spring") version "1.9.23"
+  kotlin("jvm") version "1.9.22"
+  kotlin("plugin.jpa") version "1.9.22"
+  id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+  id("com.google.cloud.tools.jib") version "3.4.1"
+  jacoco
 }
 
 configurations {
@@ -9,6 +14,11 @@ configurations {
 
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
+  implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.4.0")
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  implementation("org.flywaydb:flyway-core")
+
+  runtimeOnly("org.postgresql:postgresql")
 }
 
 kotlin {

78b427da12c6c0bc2eacd94c81004d8d346ead1f
Pankaj Khare <pankaj.khare@digital.justice.gov.uk>
2024-05-03 14:26:08 +0100

    SP2-282 add delius connectivity for case-detail

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/client/DeliusRestClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/client/DeliusRestClient.kt
@@ -3,13 +3,13 @@ package uk.gov.justice.digital.hmpps.sentenceplan.client
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
-import uk.gov.justice.digital.hmpps.sentenceplan.config.ARNSApiOauth2Config
+import uk.gov.justice.digital.hmpps.sentenceplan.config.DeliusApiOauth2Config
 import uk.gov.justice.digital.hmpps.sentenceplan.data.CaseDetail
 
 @FeignClient(
   name = "delius-api",
   url = "\${delius-api.base-url}",
-  configuration = [ARNSApiOauth2Config::class],
+  configuration = [DeliusApiOauth2Config::class],
 )
 interface DeliusRestClient {
 

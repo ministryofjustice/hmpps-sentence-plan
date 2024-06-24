ecc5811396ce65a0ffa60702a52725fb60d2fa12
Pankaj Khare <pankaj.khare@digital.justice.gov.uk>
2024-06-24 15:28:23 +0100

    revert last commit

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/data/Steps.kt
@@ -0,0 +1,9 @@
+package uk.gov.justice.digital.hmpps.sentenceplan.data
+
+import com.google.gson.annotations.SerializedName
+
+data class Steps(
+  @SerializedName("id") var id: Int? = null,
+  @SerializedName("Name") var Name: String? = null,
+  @SerializedName("Active") var Active: Boolean? = null,
+)

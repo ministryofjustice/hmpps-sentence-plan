7ca5b11c720d7517c20d7fb84a1f88d7a2838aae
Phil Wilson <github@philwilson.org>
2024-06-24 13:29:36 +0100

    Change Oasys reference field to be unique and varchar(15)

--- src/main/resources/db/migration/V4__plan.sql
+++ src/main/resources/db/migration/V4__plan.sql
@@ -16,7 +16,7 @@ create table if not exists plan
 create table if not exists oasys_pk_to_plan
 (
     id                  serial PRIMARY KEY,
-    oasys_assessment_pk text NOT NULL,
+    oasys_assessment_pk varchar(15) NOT NULL UNIQUE,
     plan_uuid           uuid NOT NULL,
     FOREIGN KEY (plan_uuid) REFERENCES plan (uuid)
 )

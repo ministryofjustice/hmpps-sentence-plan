594bf219e0054904e227b8cc2446a3f8eb5e73d0
Pankaj <92032149+pk2453@users.noreply.github.com>
2024-06-24 15:41:12 +0100

    revert last commit (#26)

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/data/AreasOfNeed.kt
@@ -0,0 +1,10 @@
+package uk.gov.justice.digital.hmpps.sentenceplan.data
+
+import com.google.gson.annotations.SerializedName
+
+data class AreasOfNeed(
+  @SerializedName("id") var id: Int? = null,
+  @SerializedName("Name") var Name: String? = null,
+  @SerializedName("active") var active: Boolean? = null,
+  @SerializedName("Goals") var Goals: ArrayList<Goals> = arrayListOf(),
+)
---
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/integration/ReferenceDataControllerTest.kt
@@ -0,0 +1,22 @@
+package uk.gov.justice.digital.hmpps.sentenceplan.integration
+
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import uk.gov.justice.digital.hmpps.sentenceplan.data.CRNLinkedRequest
+
+@AutoConfigureWebTestClient(timeout = "360000000")
+@DisplayName("Reference data Tests")
+class ReferenceDataControllerTest : IntegrationTestBase() {
+
+  private val requestBody = CRNLinkedRequest("XYZ12345")
+
+  @Test
+  fun `get question reference data should return success`() {
+    webTestClient.get().uri("/question-reference-data")
+      .header("Content-Type", "application/json")
+      .headers(setAuthorisation(user = "Tom C", roles = listOf("ROLE_RISK_INTEGRATIONS_RO")))
+      .exchange()
+      .expectStatus().isOk
+  }
+}

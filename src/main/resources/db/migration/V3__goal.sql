955d81b92dedb8c5e6a3359af722fbf918ae4d14
Phil Wilson <github@philwilson.org>
2024-07-10 11:38:03 +0100

    Initial refactoring of Area of Need to its own table

--- src/main/resources/db/migration/V3__goal.sql
+++ src/main/resources/db/migration/V3__goal.sql
@@ -3,10 +3,11 @@ create table if not exists goal
     id                  serial          PRIMARY KEY,
     uuid                uuid            NOT NULL UNIQUE,
     title               varchar(128)    NOT NULL,
-    area_of_need        varchar(128)    NOT NULL,
+    area_of_need_uuid   uuid         NOT NULL,
     target_date         timestamp       NOT NULL,
     creation_date       timestamp       NOT NULL,
     plan_uuid           uuid            NOT NULL,
     goal_order          integer         NULL,
-    FOREIGN KEY (plan_uuid) references  plan(uuid)
+    FOREIGN KEY (plan_uuid) references  plan(uuid),
+    FOREIGN KEY (area_of_need_uuid) references  area_of_need(uuid)
 );

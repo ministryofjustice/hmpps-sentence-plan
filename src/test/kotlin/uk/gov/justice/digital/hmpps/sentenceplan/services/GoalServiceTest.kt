85b0bc8c67a134cf1183013b50c09d86a8bbd337
Phil Wilson <github@philwilson.org>
2024-07-19 11:28:40 +0100

    Mostly fixes the failing test for create new steps

--- src/test/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/services/GoalServiceTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/services/GoalServiceTest.kt
@@ -3,6 +3,7 @@ package uk.gov.justice.digital.hmpps.sentenceplan.services
 import io.mockk.every
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
+import io.mockk.slot
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -10,11 +11,11 @@ import org.junit.jupiter.api.extension.ExtendWith
 import uk.gov.justice.digital.hmpps.sentenceplan.data.Step
 import uk.gov.justice.digital.hmpps.sentenceplan.data.StepActor
 import uk.gov.justice.digital.hmpps.sentenceplan.entity.AreaOfNeedRepository
+import uk.gov.justice.digital.hmpps.sentenceplan.entity.GoalEntity
 import uk.gov.justice.digital.hmpps.sentenceplan.entity.GoalRepository
 import uk.gov.justice.digital.hmpps.sentenceplan.entity.PlanRepository
 import uk.gov.justice.digital.hmpps.sentenceplan.entity.StepActorRepository
 import uk.gov.justice.digital.hmpps.sentenceplan.entity.StepActorsEntity
-import uk.gov.justice.digital.hmpps.sentenceplan.entity.StepEntity
 import uk.gov.justice.digital.hmpps.sentenceplan.entity.StepRepository
 import java.time.LocalDateTime
 import java.util.UUID
@@ -27,38 +28,52 @@ class GoalServiceTest {
   private val stepActorRepository: StepActorRepository = mockk()
   private val areaOfNeedRepository: AreaOfNeedRepository = mockk()
   private val planRepository: PlanRepository = mockk()
-  private val currentTime = LocalDateTime.now().toString()
+  private val currentTime: LocalDateTime = LocalDateTime.parse("2019-12-15T15:14:21.629")
   private val goalService = GoalService(goalRepository, stepRepository, stepActorRepository, areaOfNeedRepository, planRepository)
-  private val uuid = UUID.fromString("ef74ee4b-5a0b-481b-860f-19187260f2e7")
+  private val goalUuid = UUID.fromString("ef74ee4b-5a0b-481b-860f-19187260f2e7")
 
-  private val stepEntity = StepEntity(
-    description = "description",
-    id = 123L,
-    status = "status",
-    goal = null,
-    creationDate = currentTime,
+  private val goalEntityNoSteps: GoalEntity = GoalEntity(
+    title = "Mock Goal",
+    areaOfNeedUuid = UUID.randomUUID(),
+    plan = null,
+    uuid = goalUuid,
   )
+
   private val actorsEntityList = listOf(
     StepActorsEntity(1, UUID.fromString("71793b64-545e-4ae7-9936-610639093857"), "actor", 1),
   )
   private val actors = listOf(
     StepActor("actor", 1),
   )
-  private val steps = Step(
-    description = "description",
-    status = "status",
-    actor = actors,
+  private val steps = listOf(
+    Step(
+      description = "description 1",
+      status = "status 1",
+      actor = actors,
+    ),
+    Step(
+      description = "description 2",
+      status = "status 2",
+      actor = actors,
+    ),
   )
 
   @Test
   fun `create new steps`() {
-    every { stepRepository.save(any<StepEntity>()) } returns stepEntity
-    every { stepActorRepository.saveAll(any<List<StepActorsEntity>>()) } returns actorsEntityList
-    val stepsList = goalService.createNewSteps(uuid, listOf(steps)).steps!!
-    assertThat(stepsList.get(0).status).isEqualTo("status")
-    assertThat(stepsList.get(0).id).isEqualTo(123)
-    assertThat(stepsList.get(0).goal?.uuid).isEqualTo(uuid)
-    assertThat(stepsList.get(0).description).isEqualTo("description")
-    assertThat(stepsList.get(0).creationDate).isEqualTo(currentTime)
+    val goalSlot = slot<GoalEntity>()
+    every { goalRepository.findByUuid(goalUuid) } returns goalEntityNoSteps
+    every { goalRepository.save(capture(goalSlot)) } answers { goalSlot.captured }
+
+    val stepsList = goalService.createNewSteps(goalUuid, steps).steps!!
+
+    assertThat(stepsList.size).isEqualTo(2)
+
+    assertThat(stepsList.get(0).status).isEqualTo("status 1")
+    assertThat(stepsList.get(0).goal?.uuid).isEqualTo(goalUuid)
+    assertThat(stepsList.get(0).description).isEqualTo("description 1")
+
+    assertThat(stepsList.get(1).status).isEqualTo("status 2")
+    assertThat(stepsList.get(1).goal?.uuid).isEqualTo(goalUuid)
+    assertThat(stepsList.get(1).description).isEqualTo("description 2")
   }
 }

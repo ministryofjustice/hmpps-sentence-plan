59515285399ea7d48203c25186fc2a386caf9d91
Alex Warhurst <alex.warhurst@digital.justice.gov.uk>
2024-08-16 16:00:58 +0100

    Update test data, and remove unused imports

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/controller/PlanController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/controller/PlanController.kt
@@ -2,8 +2,6 @@ package uk.gov.justice.digital.hmpps.sentenceplan.controller
 
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping

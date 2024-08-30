031acb810efd2cb0199f3b31211e4e28b8df42db
alexw-moj <alex.warhurst@digital.justice.gov.uk>
2024-08-30 10:13:34 +0100

    Update Step classes to use an enum for the status field (#77)
    * Update Step classes to use an enum for the status field
    
    * Remove unused import
    
    * Minor rework
    
    (cherry picked from commit 7ef922cc90961f2e6809c4258b4bf0ea11730586)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/data/Step.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/data/Step.kt
@@ -1,7 +1,9 @@
 package uk.gov.justice.digital.hmpps.sentenceplan.data
 
+import uk.gov.justice.digital.hmpps.sentenceplan.entity.StepStatus
+
 data class Step(
   val description: String,
-  val status: String,
+  val status: StepStatus,
   val actor: String,
 )

de29b2976edf145c5e204d452d17807d75930576
Phil Wilson <pipwilson@users.noreply.github.com>
2024-09-25 15:33:41 +0100

    Updates the relationships between PlanEntity, PlanAgreementNoteEntity and PlanProgressNoteEntity to be object relationships (#91)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/entity/PlanProgressNoteEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/entity/PlanProgressNoteEntity.kt
@@ -3,14 +3,16 @@ package uk.gov.justice.digital.hmpps.sentenceplan.entity
 import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
 import jakarta.persistence.Table
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.Instant
 import java.time.format.DateTimeFormatter
-import java.util.UUID
 
 enum class NoteIsSupportNeeded {
   YES,
@@ -27,8 +29,10 @@ class PlanProgressNoteEntity(
   @JsonIgnore
   val id: Long? = null,
 
-  @Column(name = "plan_uuid")
-  var planUuid: UUID,
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "plan_id", referencedColumnName = "id")
+  @JsonIgnore
+  val plan: PlanEntity = PlanEntity(),
 
   @Column(name = "note")
   var note: String,

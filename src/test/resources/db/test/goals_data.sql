6fd1bd57d30d48a6a7a9011112babc98976b151c
Phil Wilson <github@philwilson.org>
2024-09-27 17:26:34 +0100

    Fix tests so they work with GoalEntity.targetDate being a date and no time.

--- src/test/resources/db/test/goals_data.sql
+++ src/test/resources/db/test/goals_data.sql
@@ -3,7 +3,7 @@ INSERT INTO goal (uuid, title, area_of_need_id, target_date, created_date, creat
 SELECT '31d7e986-4078-4f5c-af1d-115f9ba3722d',
        'Goal For Now Title',
        aon.id,
-       '2024-06-27 16:10:57.299363',
+       '2024-06-27',
        now(),
        practitioner.id,
        now(),

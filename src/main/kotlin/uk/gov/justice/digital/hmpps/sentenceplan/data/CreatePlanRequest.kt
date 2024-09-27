ea90a9a0edf89ad5d47cbf56c8dec3841534289d
Alex Warhurst <alex.warhurst@digital.justice.gov.uk>
2024-09-27 15:26:15 +0100

    Adding UserDetails, as these will also be passed

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/data/CreatePlanRequest.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/data/CreatePlanRequest.kt
@@ -4,5 +4,6 @@ import uk.gov.justice.digital.hmpps.sentenceplan.entity.PlanType
 
 data class CreatePlanRequest(
   val planType: PlanType,
+  val userDetails: UserDetails,
 )
 // Maybe it includes Auditing information...

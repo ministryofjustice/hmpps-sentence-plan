4017c69d45eb0c4f54ebbd04b167e11911139960
Phil Wilson <github@philwilson.org>
2024-10-16 16:28:15 +0100

    Initial attempt to add notes to Goal

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/entity/GoalNoteEntity.kt
@@ -0,0 +1,57 @@
+package uk.gov.justice.digital.hmpps.sentenceplan.entity
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.EnumType
+import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+import jakarta.persistence.Table
+import org.springframework.data.annotation.CreatedBy
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@Entity(name = "GoalNote")
+@Table(name = "goal_notes")
+@EntityListeners(AuditingEntityListener::class)
+class GoalNoteEntity(
+  @Id
+  @Column(name = "id")
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @JsonIgnore
+  var id: Long? = null,
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "goal_id", referencedColumnName = "id")
+  @JsonIgnore
+  var goal: GoalEntity? = null,
+
+  @Column(name = "note")
+  var note: String,
+
+  @Column(name = "note_type")
+  @Enumerated(EnumType.STRING)
+  var type: NoteType = NoteType.PROGRESS,
+
+  @CreatedDate
+  @Column(name = "created_date")
+  val createdDate: LocalDateTime = LocalDateTime.now(),
+
+  @CreatedBy
+  @ManyToOne
+  @JoinColumn(name = "created_by_id")
+  var createdBy: PractitionerEntity? = null,
+)
+
+enum class NoteType {
+  PROGRESS,
+  REMOVED,
+  ACHIEVED,
+}

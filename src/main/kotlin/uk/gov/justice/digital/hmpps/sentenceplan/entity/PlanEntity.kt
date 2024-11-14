f5e15e97ae1a2d5ec7c302dcaf77bc621ce80ff1
Phil Wilson <pipwilson@users.noreply.github.com>
2024-11-14 11:21:18 +0000

    SP2-922 retrieve plan and goal notes (#137)
    Adds new objects and queries to handle returning Notes through the API

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/entity/PlanEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/entity/PlanEntity.kt
@@ -3,6 +3,8 @@ package uk.gov.justice.digital.hmpps.sentenceplan.entity
 import com.fasterxml.jackson.annotation.JsonFormat
 import com.fasterxml.jackson.annotation.JsonIgnore
 import jakarta.persistence.Column
+import jakarta.persistence.ColumnResult
+import jakarta.persistence.ConstructorResult
 import jakarta.persistence.Entity
 import jakarta.persistence.EntityListeners
 import jakarta.persistence.EnumType
@@ -12,7 +14,9 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import jakarta.persistence.NamedNativeQuery
 import jakarta.persistence.OneToOne
+import jakarta.persistence.SqlResultSetMapping
 import jakarta.persistence.Table
 import jakarta.transaction.Transactional
 import org.springframework.data.annotation.CreatedBy
@@ -24,6 +28,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import uk.gov.justice.digital.hmpps.sentenceplan.data.Note
 import uk.gov.justice.digital.hmpps.sentenceplan.exceptions.NotFoundException
 import java.time.LocalDateTime
 import java.util.UUID
@@ -31,6 +36,65 @@ import java.util.UUID
 @Entity
 @Table(name = "plan")
 @EntityListeners(AuditingEntityListener::class)
+@NamedNativeQuery(
+  name = "PlanEntity.getPlanAndGoalNotes",
+  query = """
+select 'Plan' as note_object,
+       plan_notes.agreement_status_note as note,
+       plan_notes.optional_note as additional_note,
+       CAST(plan_notes.agreement_status AS VARCHAR) AS note_type,
+       null as goal_title,
+       null as goal_uuid,
+       null as goal_status,
+       plan_notes.created_date,
+       practitioner.username as created_by
+from "sentence-plan".plan_agreement_notes plan_notes
+    inner join "sentence-plan".practitioner on plan_notes.created_by_id = practitioner.id
+    inner join "sentence-plan".plan_version on plan_notes.plan_version_id = plan_version.id
+    inner join "sentence-plan".plan on plan.current_plan_version_id = plan_version.id
+    and plan.uuid = ?1
+
+UNION ALL
+
+select 'Goal' as note_object,
+       goal_notes.note,
+       null as additional_note,
+       CAST(goal_notes.note_type AS VARCHAR) AS note_type,
+       goal.title as goal_title,
+       CAST(goal.uuid AS VARCHAR) as goal_uuid, 
+       CAST(goal.goal_status AS VARCHAR) as goal_status,
+       goal_notes.created_date,
+       practitioner.username as created_by
+from "sentence-plan".goal_notes
+    inner join "sentence-plan".practitioner on goal_notes.created_by_id = practitioner.id
+    inner join "sentence-plan".goal on goal_notes.goal_id = goal.id
+    inner join "sentence-plan".plan_version on goal.plan_version_id = plan_version.id
+    inner join "sentence-plan".plan on plan.current_plan_version_id = plan_version.id
+    and plan.uuid = ?1
+        
+    ORDER BY created_date DESC;
+    """,
+  resultSetMapping = "NoteMapping",
+)
+@SqlResultSetMapping(
+  name = "NoteMapping",
+  classes = [
+    ConstructorResult(
+      targetClass = Note::class,
+      columns = [
+        ColumnResult(name = "note_object"),
+        ColumnResult(name = "note"),
+        ColumnResult(name = "additional_note"),
+        ColumnResult(name = "note_type"),
+        ColumnResult(name = "goal_title"),
+        ColumnResult(name = "goal_uuid"),
+        ColumnResult(name = "goal_status"),
+        ColumnResult(name = "created_date", type = LocalDateTime::class),
+        ColumnResult(name = "created_by"),
+      ],
+    ),
+  ],
+)
 class PlanEntity(
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -87,6 +151,9 @@ interface PlanRepository : JpaRepository<PlanEntity, Long> {
   """,
   )
   fun findPlanByUuid(planUuid: UUID): PlanEntity?
+
+  @Query(nativeQuery = true)
+  fun getPlanAndGoalNotes(planUuid: UUID): List<Note>
 
   @Query("select p.* from plan p inner join oasys_pk_to_plan o on p.id = o.plan_id and o.oasys_assessment_pk = :oasysAssessmentPk", nativeQuery = true)
   fun findByOasysAssessmentPk(@Param("oasysAssessmentPk") oasysAssessmentPk: String): PlanEntity?
---
+++ src/test/resources/db/test/goal_notes_cleanup.sql
@@ -0,0 +1,1 @@
+DELETE FROM goal_notes;

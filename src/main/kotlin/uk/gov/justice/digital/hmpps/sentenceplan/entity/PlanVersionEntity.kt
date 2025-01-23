60c1701d7ffd4926a577e4f67a416f0c08860c17
Phil Wilson <github@philwilson.org>
2025-01-23 12:43:54 +0000

    Ensures timestamps provided to API clients are always the same.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/entity/PlanVersionEntity.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/entity/PlanVersionEntity.kt
@@ -133,6 +133,7 @@ class PlanVersionEntity(
   var softDeleted: Boolean = false,
 
   @Formula("GREATEST(last_updated_date, (SELECT MAX(g.last_updated_date) FROM goal g WHERE g.plan_version_id = id))")
+  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
   var mostRecentUpdateDate: LocalDateTime? = null,
 
   // this query retrieves the username directly because Formula can only retrieve scalar values, not objects.

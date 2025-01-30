0bf7ce8f8344f90137db2aaa127b68d6b1ff7007
Phil Wilson <github@philwilson.org>
2025-01-30 17:50:29 +0000

    This change allows commands from the co-ordinator which include the actioning user in the body to override the values in the authentication header.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/config/JpaAuditConfig.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/config/JpaAuditConfig.kt
@@ -1,11 +1,16 @@
 package uk.gov.justice.digital.hmpps.sentenceplan.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.dao.EmptyResultDataAccessException
 import org.springframework.data.domain.AuditorAware
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.ServletRequestAttributes
+import org.springframework.web.util.ContentCachingRequestWrapper
+import uk.gov.justice.digital.hmpps.sentenceplan.data.UserDetails
 import uk.gov.justice.digital.hmpps.sentenceplan.entity.PractitionerEntity
 import uk.gov.justice.digital.hmpps.sentenceplan.entity.PractitionerRepository
 import java.util.Optional
@@ -16,20 +21,37 @@ class JpaAuditConfig(private val practitionerRepository: PractitionerRepository)
 
   @Bean
   fun auditorProvider(): AuditorAware<PractitionerEntity> = AuditorAware<PractitionerEntity> {
+    val requestAttributes = RequestContextHolder.getRequestAttributes() as ServletRequestAttributes?
+    val request = requestAttributes?.request
+
+    var externalId = ""
+    var username = ""
     var practitionerEntity: PractitionerEntity
 
-    val authenticationName = SecurityContextHolder.getContext().authentication.name
+    if (request != null && request is ContentCachingRequestWrapper) {
+      val userDetails = request.let { readUserDetailsFromRequest(it) }
 
-    val usernameParts = authenticationName.split('|')
-    val externalId: String
-    val username: String
+      if (userDetails != null && userDetails.id.isNotEmpty() && userDetails.name.isNotEmpty()) {
+        externalId = userDetails.id
+        username = userDetails.name
+      }
 
-    if (usernameParts.size != 2) {
-      externalId = "SYSTEM"
-      username = usernameParts[0]
-    } else {
-      externalId = usernameParts[0]
-      username = usernameParts[1]
+      println("External ID: $externalId")
+      println("Username: $username")
+    }
+
+    if (externalId.isEmpty()) {
+      val authenticationName = SecurityContextHolder.getContext().authentication.name
+
+      val usernameParts = authenticationName.split('|')
+
+      if (usernameParts.size != 2) {
+        externalId = "SYSTEM"
+        username = usernameParts[0]
+      } else {
+        externalId = usernameParts[0]
+        username = usernameParts[1]
+      }
     }
 
     try {
@@ -43,5 +65,20 @@ class JpaAuditConfig(private val practitionerRepository: PractitionerRepository)
     }
 
     Optional.of(practitionerEntity)
+  }
+
+  fun readUserDetailsFromRequest(request: ContentCachingRequestWrapper): UserDetails? {
+    val requestBody = request.contentAsString.let {
+      ObjectMapper().readTree(it)
+    }
+
+    val userDetailsNode = requestBody.get("userDetails")
+
+    return userDetailsNode?.let {
+      UserDetails(
+        id = it.get("id").asText(),
+        name = it.get("name").asText(),
+      )
+    }
   }
 }

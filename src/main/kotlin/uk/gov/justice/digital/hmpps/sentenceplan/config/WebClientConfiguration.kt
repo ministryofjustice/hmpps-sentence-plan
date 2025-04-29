f114b1c6dec5db0df3a67e4521ddb1679ddd518c
Phil Wilson <github@philwilson.org>
2025-04-29 17:55:45 +0100

    Remove direct call to ARNS service - this is not used.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/config/WebClientConfiguration.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/config/WebClientConfiguration.kt
@@ -10,13 +10,9 @@ import java.time.Duration
 
 @Configuration
 class WebClientConfiguration(
-  @Value("\${arns-api.base-url}") val arnsApiBaseUrl: String,
   @Value("\${delius-api.base-url}") val deliusApiBaseUrl: String,
   @Value("\${api.timeout:20s}") val timeout: Duration,
 ) {
-  @Bean
-  fun arnsRestClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient = builder.authorisedWebClient(authorizedClientManager, registrationId = "arns-api", url = arnsApiBaseUrl, timeout)
-
   @Bean
   fun deliusRestClient(authorizedClientManager: OAuth2AuthorizedClientManager, builder: WebClient.Builder): WebClient = builder.authorisedWebClient(authorizedClientManager, registrationId = "delius-api", url = deliusApiBaseUrl, timeout)
 }
--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/controller/PopInfoController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/controller/PopInfoController.kt
@@ -7,28 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import uk.gov.justice.digital.hmpps.sentenceplan.data.CRNLinkedRequest
 import uk.gov.justice.digital.hmpps.sentenceplan.data.PopInfoResponse
-import uk.gov.justice.digital.hmpps.sentenceplan.data.RiskAssessmentResponse
-import uk.gov.justice.digital.hmpps.sentenceplan.services.ARNSApiService
+import uk.gov.justice.digital.hmpps.sentenceplan.services.DeliusApiService
 
 @RestController
 @RequestMapping("/info/pop")
 @PreAuthorize("hasAnyRole('ROLE_RISK_INTEGRATIONS_RO')")
 class PopInfoController(
-  private val arnsApiService: ARNSApiService,
+  private val deliusApiService: DeliusApiService,
 ) {
   @PostMapping
   fun getPopInfo(
     @RequestBody body: CRNLinkedRequest,
   ): PopInfoResponse {
     val crn = body.crn
-    return arnsApiService.getPopInfo(crn)
-  }
-
-  @PostMapping("/scores/risk")
-  fun getRiskScore(
-    @RequestBody body: CRNLinkedRequest,
-  ): RiskAssessmentResponse? {
-    val crn = body.crn
-    return arnsApiService.getRiskScoreInfoByCrn(crn)
+    return deliusApiService.getPopInfo(crn)
   }
 }

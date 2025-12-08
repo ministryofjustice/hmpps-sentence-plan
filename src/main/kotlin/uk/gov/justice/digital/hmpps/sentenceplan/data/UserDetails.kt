cbeacc81d26f642d5f6f391fe84c1303890ab97d
NicolaWard-SBG <nicola.ward@justice.gov.uk>
2025-12-08 11:04:59 +0000

    Revert "ACT-106: Temporary fix to handle requests with Location in the payload (#232)"
    This reverts commit f06d824bb36f09d82a97b0bb1c70e588e0ebf9e3.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/data/UserDetails.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/sentenceplan/data/UserDetails.kt
@@ -3,10 +3,4 @@ package uk.gov.justice.digital.hmpps.sentenceplan.data
 data class UserDetails(
   val id: String,
   val name: String,
-  val location: Location? = null,
 )
-
-enum class Location {
-  PRISON,
-  COMMUNITY,
-}
